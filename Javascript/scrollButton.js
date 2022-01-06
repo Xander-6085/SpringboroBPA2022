adc0e8c30bd2fde203d6464a3609aa6666823c8f
Xander-6085 <53024030+Xander-6085@users.noreply.github.com>
2022-01-06 08:08:01 -0500

    Update scrollButton.js

--- Javascript/scrollButton.js
+++ Javascript/scrollButton.js
@@ -1,15 +1,14 @@
 //scroll to fuction
 function scrollUp() {
     window.scrollTo({top: 0, behavior: 'smooth'});
-    console.log("hi");
 }
 const bcheckpoint = 750;
 let button = document.querySelector(".scrollUp");
 window.addEventListener("scroll", () => {
 	const currentScroll = window.pageYOffset;
     if (currentScroll >= bcheckpoint) {
-      button.style.opacity = "1";
-    } else {
       button.style.opacity = "inherit";
+    } else {
+      button.style.opacity = "0";
     }
 });
