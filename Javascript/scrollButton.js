312ef5a80d9d45c06d48bb2d2fc3901453d0cc3d
Xander-6085 <xander.lewis22@gmail.com>
2022-01-06 17:05:25 -0500

    Adding properly working scroll to top button on every current page

--- Javascript/scrollButton.js
+++ Javascript/scrollButton.js
@@ -1,14 +1,34 @@
+let button = document.querySelector(".scrollUp");
+let currentopacity = "0.5";
+let visible = false;
+const bcheckpoint = 750;
+
 //scroll to fuction
 function scrollUp() {
     window.scrollTo({top: 0, behavior: 'smooth'});
+    currentopacity = "0.5"
 }
-const bcheckpoint = 750;
-let button = document.querySelector(".scrollUp");
+
+function hoverOpacity (direction) {
+    if(visible){
+	if(direction){
+            currentopacity = "1";
+	    button.style.opacity = currentopacity;
+	}
+        else {
+	    currentopacity = "0.5";
+	    button.style.opacity = "0.5";
+        }
+    }
+}
+
 window.addEventListener("scroll", () => {
 	const currentScroll = window.pageYOffset;
     if (currentScroll >= bcheckpoint) {
-      button.style.opacity = "0.5";
+      button.style.opacity = currentopacity;
+      visible = true;
     } else {
       button.style.opacity = "0";
+      visible = false;
     }
 });
